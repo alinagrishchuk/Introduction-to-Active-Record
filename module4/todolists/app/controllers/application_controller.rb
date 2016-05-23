205cd7217c1d25fc97af9effe96ecf8fba1f866e
alinagrishchuk <alina.grishchuk91@gmail.com>
2016-05-23 17:58:14 +0300

    add authentication and pagination

--- module4/todolists/app/controllers/application_controller.rb
+++ module4/todolists/app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
+  before_action :ensure_login
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  helper_method :logged_in?, :current_user
   protect_from_forgery with: :exception
+  def logged_in?
+    session[:user_id]
+  end
+
+  def current_user
+    @current_user ||= User.find_by_id(session[:user_id])
+  end
+
+  def ensure_login
+    redirect_to login_path unless logged_in?
+  end
 end
